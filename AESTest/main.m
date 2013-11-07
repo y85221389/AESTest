080efaa09ee724028910b305fb71ba8793dab8b3
惠一路 <HYL@server.local>
2013-11-07 17:31:07 +0800

    Initial Commit

---
+++ AESTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AESTest
+//
+//  Created by 惠一路 on 13-11-7.
+//  Copyright (c) 2013年 惠一路. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
